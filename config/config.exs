81b8a1cb26fc5515703fe1e4dd232d8029d0e461
An Duong <duonghienan.dha@gmail.com>
2020-04-18 16:58:38 +0700

    Generate the phoneix app with mix phx.new.web --no-ecto

--- config/config.exs
+++ config/config.exs
@@ -9,6 +9,17 @@
 # move said applications out of the umbrella.
 import Config
 
+config :rsvp_web,
+  generators: [context_app: false]
+
+# Configures the endpoint
+config :rsvp_web, RsvpWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "C9FwR3TVkhJJVl50bmIDImNNGmSBZz3XmWfAQpGJDixGA06lB0BjPok4JOPT1VVH",
+  render_errors: [view: RsvpWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: RsvpWeb.PubSub, adapter: Phoenix.PubSub.PG2],
+  live_view: [signing_salt: "lQDucxJW"]
+
 # Sample configuration:
 #
 #     config :logger, :console,
@@ -16,3 +27,15 @@ import Config
 #       format: "$date $time [$level] $metadata$message\n",
 #       metadata: [:user_id]
 #
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
